0c0167f0d9f99bd6331119d8f7f10ac5cfe2d5af
Andrew Perv <82667860+Pandnak@users.noreply.github.com>
2025-06-15 20:50:57 +0300

    Update MLP_policy.py
    forward and update are implemented

--- hw1/cs285/policies/MLP_policy.py
+++ hw1/cs285/policies/MLP_policy.py
@@ -129,7 +129,9 @@ class MLPPolicySL(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
         # through it. For example, you can return a torch.FloatTensor. You can also
         # return more flexible objects, such as a
         # `torch.distributions.Distribution` object. It's up to you!
-        raise NotImplementedError
+        # raise NotImplementedError
+        out = self.mean_net(observation)
+        return out
 
     def update(self, observations, actions):
         """
@@ -141,7 +143,8 @@ class MLPPolicySL(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
             dict: 'Training Loss': supervised learning loss
         """
         # TODO: update the policy and return the loss
-        loss = TODO
+        TrainingLoss = torch.nn.MSELoss
+        loss = TrainingLoss(self.mean_net(observations), actions)
         return {
             # You can add extra logging information here, but keep this line
             'Training Loss': ptu.to_numpy(loss),
